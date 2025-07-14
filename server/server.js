c29c771931c3d8f2c4995a01f3555b4a56dbe571
Akshat Chaturvedi <akshatchaturvedi991@gmail.com>
2025-07-14 17:32:57 +0530

    final

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-frontend-seven-iota.vercel.app']
 
 //app.post('/stripe',express.raw({type: 'application/json'}),stripeWebHooks)
 // app.post(
